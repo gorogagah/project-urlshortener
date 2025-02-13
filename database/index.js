2943e738a31561343f3f7bf9ea79c17f1938af91
Anggoro Gagah <aak270@gmail.com>
2025-02-13 21:12:41 +0700

    add posrgresql

---
+++ database/index.js
@@ -0,0 +1,33 @@
+const pgp = require('pg-promise')(/* options */)
+const db = pgp(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_IP}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+
+exports.storeUrl = function(shortId, originalUrl){
+    return db.one('INSERT INTO urls(short_id, original_url) VALUES($1, $2) RETURNING *', [shortId, originalUrl]);
+}
+
+exports.urlExists = async function(shortId){
+    try{
+        const urls = await db.any('SELECT original_url FROM urls WHERE short_id = $1', [shortId]);
+        if(urls.length > 0){
+            console.log(`url retrieved ${urls[0]}`);
+            return true;
+        }
+        
+        console.log("url is empty");
+    }catch(error){
+        console.log(`error on retrieving url: ${error}`);
+    }
+
+    return false;
+}
+
+exports.getOriginalUrl = async function(shortId){
+    try{
+        const url = await db.one('SELECT original_url FROM urls WHERE short_id = $1', [shortId]);
+        return url.original_url;
+    }catch(error){
+        console.log(`error on retrieving url: ${error}`);
+    }
+
+    return "";
+}
--- index.js
+++ index.js
@@ -1,52 +1,85 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const dns = require('node:dns');
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express();
+const bodyParser = require('body-parser');
 
+const { customAlphabet } = require("nanoid");
+
+const db = require("./database/index");
+
+const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.use(cors());
+const pattern = new RegExp(
+  '^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$', // fragment locator
+  'i'
+);
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
-const savedUrl = [];
-
-app.post("/api/shorturl", function(req, res){
+// Create short link
+app.post("/shorten", async function(req, res){
   const url = req.body["url"];
-
-  let tempDnsUrl = url.slice(url.indexOf("//") + 2);
-  let slashIndex = tempDnsUrl.indexOf("/");
-  var dnsUrl = slashIndex < 0 ? tempDnsUrl : tempDnsUrl.slice(0, slashIndex); 
+  if(!url){
+    return res.json({ error: "invalid url", message: "url is empty" });
+  }
   
-  dns.lookup(dnsUrl, function(err, address){
-    if(err && !url.includes("localhost")){
-      res.json({ error: 'invalid url' });
-    }else{
-      savedUrl[savedUrl.length + 1] = url;
-      res.json({ original_url : url, short_url : savedUrl.length-1});
-    }
+  // Check validity
+  if(!pattern.test(url)){
+    return res.json({ error: "invalid url", url: url });
+  }
+
+  // Create short url
+  let shortId = await createUrl();
+  if(shortId == ""){
+    console.log(`too many attempts to create short url`);
+    return res.json({ error: "attempt limit", message: "failed to create short url many times"});
+  }
+
+  db.storeUrl(shortId, url).then((data) => {
+    console.log(`url stored: ${data}`);
+    return res.json({ original_url: url, short_id: shortId });
+  }).catch((error) => {
+    console.log(`error on storing url: ${error}`);
+    return res.json({ error: "database insert", message: "failed to store url to database"});
   });
 });
 
-app.get("/api/shorturl/:short_url", function(req, res){
-  const short_url = req.params.short_url;
-  res.redirect(savedUrl[short_url]);
+async function createUrl(){
+  let size = 6;
+  let attempt = 0;
+
+  while (true) {
+    let shortUrl = nanoid(size);
+    if(!(await db.urlExists(shortUrl))){
+      return shortUrl;
+    }
+
+    attempt++;
+    if(attempt % 2 == 0 && size <= 10){
+      size++;
+    }
+
+    if(attempt > 100){
+      return "";
+    }
+  }
+}
+
+app.get("/:short_id", async function(req, res){
+  const url = await db.getOriginalUrl(req.params.short_id);
+  res.redirect(url);
 });
 
 app.listen(port, function() {
